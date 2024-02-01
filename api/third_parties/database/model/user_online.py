91c83227f96f81ec194e0834ea2bba8aa706a8f8
xinhnh <xinhkg00@gmail.com>
2024-02-01 22:13:39 +0700

    socket, user online

--- api/third_parties/database/model/user_online.py
+++ api/third_parties/database/model/user_online.py
@@ -5,10 +5,10 @@ from api.third_parties.database.model.base import BaseModel
 
 class UserOnline(BaseModel):
 
-    def __init__(self, user_online_code=None, user_id=None, socket_id=None, status=None):
+    def __init__(self, user_online_code=None, user_code=None, socket_id=None, status=True):
         super().__init__()
         self.user_online_code = user_online_code,
-        self.user_id = user_id,
+        self.user_code = user_code,
         self.socket_id = socket_id,
         self.status = status
 
