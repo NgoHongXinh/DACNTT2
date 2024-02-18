fa4e4e254b9e1f6db158796e4ccee4d6d3943e35
xinhnh <xinhkg00@gmail.com>
2024-02-18 23:34:06 +0700

    ko tra ve thong tin user

--- api/endpoint/authen/schema.py
+++ api/endpoint/authen/schema.py
@@ -6,7 +6,7 @@ from api.endpoint.user.schema import ResponseUser
 class ResponseToken(BaseModel):
     access_token: str
     token_type: str = Field(default="bearer")
-    user_info: ResponseUser
+    # user_info: ResponseUser
 
 
 class RequestInfoToken(BaseModel):
